a32871fe91de9bef28d7825903512c371ac84a31
2017A7PS0110G <f20170110@goa.bits-pilani.ac.in>
2021-06-16 18:21:30 +0530

    Commit26: Removed dependency for mongodb, Refactored ExceptionController to include InternalServiceException and more return relevant HTTPStatus codes

--- src/main/java/com/electionController/controllers/ExceptionController.java
+++ src/main/java/com/electionController/controllers/ExceptionController.java
@@ -1,9 +1,10 @@
 package com.electionController.controllers;
 
+import com.electionController.exceptions.InternalServiceException;
 import com.electionController.exceptions.EntityNotFoundException;
-import com.electionController.exceptions.InvalidCredentialException;
 import com.electionController.exceptions.InvalidParameterException;
 import com.electionController.exceptions.RestrictedActionException;
+import com.electionController.exceptions.InvalidCredentialException;
 import com.electionController.structures.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,10 +16,10 @@ import org.springframework.web.client.HttpClientErrorException;
 @ControllerAdvice
 public class ExceptionController {
     @ExceptionHandler(EntityNotFoundException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    public Response processException(EntityNotFoundException ex) {
-        return new Response.Builder()
+    public Response processException(final EntityNotFoundException ex) {
+        return Response.Builder()
                 .withResponse(EntityNotFoundException.class)
                 .withStatusCode(ex.getErrorCode())
                 .withStatus(ex.getErrorMessage())
@@ -26,10 +27,10 @@ public class ExceptionController {
     }
 
     @ExceptionHandler(RestrictedActionException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     @ResponseBody
-    public Response processException(RestrictedActionException ex) {
-        return new Response.Builder()
+    public Response processException(final RestrictedActionException ex) {
+        return Response.Builder()
                 .withResponse(RestrictedActionException.class)
                 .withStatusCode(ex.getErrorCode())
                 .withStatus(ex.getErrorMessage())
@@ -37,10 +38,10 @@ public class ExceptionController {
     }
 
     @ExceptionHandler(InvalidCredentialException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ResponseBody
-    public Response processException(InvalidCredentialException ex) {
-        return new Response.Builder()
+    public Response processException(final InvalidCredentialException ex) {
+        return Response.Builder()
                 .withResponse(InvalidCredentialException.class)
                 .withStatusCode(ex.getErrorCode())
                 .withStatus(ex.getErrorMessage())
@@ -48,11 +49,22 @@ public class ExceptionController {
     }
 
     @ExceptionHandler(InvalidParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public Response processException(final InvalidParameterException ex) {
+        return Response.Builder()
+                .withResponse(InvalidParameterException.class)
+                .withStatusCode(ex.getErrorCode())
+                .withStatus(ex.getErrorMessage())
+                .build();
+    }
+
+    @ExceptionHandler(InternalServiceException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
-    public Response processException(InvalidParameterException ex) {
-        return new Response.Builder()
-                .withResponse(InvalidParameterException.class)
+    public Response processException(final InternalServiceException ex) {
+        return Response.Builder()
+                .withResponse(InternalServiceException.class)
                 .withStatusCode(ex.getErrorCode())
                 .withStatus(ex.getErrorMessage())
                 .build();
